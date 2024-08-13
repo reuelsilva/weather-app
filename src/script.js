4d578c85449881048eeadfcc828e20c4a277fcc8
reuelsilva <reuelsilva19@gmail.com>
2024-08-13 09:47:15 -0300

    feat: adicionado parâmetros de unidade e idioma à requisição da API do OpenWeatherMap
    - Adiciona o parâmetro `units=metric` para retornar a temperatura em Celsius.
    - Adiciona o parâmetro `lang=pt_br` para exibir as descrições do tempo em português.

--- src/script.js
+++ src/script.js
@@ -32,7 +32,7 @@ const fetchingData = async () => {
 }
 
 const getUserLocationData = async (coords) => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&units=metric&lang=pt_br&appid=${API_KEY}`);
     const data = await response.json();
     if(data){
         document.querySelector(".required-location-message").style.display = "none";
